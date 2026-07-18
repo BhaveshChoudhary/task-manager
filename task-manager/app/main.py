d0be56cc44fdf69360c30a31fe878ccf6f1d7313
bhavesh <bhaveshpchoudhary1991@gmail.com>
2026-07-18 13:23:08 -0400

    Updated main.py

---
+++ task-manager/app/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastApi
+
+app = FastApi()
+
+tasks = [
+    {"id": 1, "name": "Learn Docker"},
+    {"id": 2, "name": "Learn kubernetes"}
+]
+
+@app.get("/")
+def home():
+    return {
+        "message": "Task Manager API running"
+    }
+
+@app.get("/tasks")
+def get_tasks():
+    return tasks    
+
+@app.post("/tasks")
+def add_task(task: dict):
+    tasks.append(task)
+    return task
